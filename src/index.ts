79d756500cf831a22c2e97ba760b0bc32424765b
Timelink <85010918+Taolee-crypto@users.noreply.github.com>
2025-12-03 21:04:22 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,14 @@ export default {
     const url = new URL(request.url);
 
     if (url.pathname === '/api/uuid') {
+      // 임의 UUID 생성
       const uuid = crypto.randomUUID();
       return new Response(JSON.stringify({ uuid }), {
         headers: { 'Content-Type': 'application/json' },
       });
     }
 
-    // 정적 자산 처리
+    // 정적 파일 처리
     return await getAssetFromKV(request);
   }
 };
