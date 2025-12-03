c0d9444391bd1b7fd13bcc2320bbf1b747d91c34
Timelink <85010918+Taolee-crypto@users.noreply.github.com>
2025-12-04 01:22:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-import { randomUUID } from "crypto";
-import http from "http";
-
-const server = http.createServer((req, res) => {
-  const data = { uuid: randomUUID() };
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(data));
-});
-
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
+export default {
+  async fetch(request) {
+    return new Response(JSON.stringify({ uuid: crypto.randomUUID() }), {
+      headers: { "Content-Type": "application/json" }
+    });
+  }
+};
