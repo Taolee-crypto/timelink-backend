6d4f2b69adba2c8b07215151d535a4d323e8947b
Taolee-crypto <taolee@kakao.com>
2025-12-15 13:16:23 +0900

    업데이트: 전체 페이지 연결 완료

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+// 백엔드 공통 상수 (프론트와 동일)
+module.exports = {
+  DISTRIBUTION_MODEL: {
+    CREATOR: 30,
+    USER: 50,
+    PLATFORM: 20
+  },
+  FILE_TYPES: {
+    TL3: 'tl3',
+    TL4: 'tl4'
+  },
+  USER_ROLES: {
+    UPLOADER: 'uploader',
+    COPYRIGHT_OWNER: 'copyright_owner',
+    USER: 'user'
+  }
+};
